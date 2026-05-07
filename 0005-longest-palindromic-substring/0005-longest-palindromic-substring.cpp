906ebecf1c0646eefd20b99502edca4ff805e852
jonathan-cpp <130759463+jonathan-cpp@users.noreply.github.com>
2026-05-07 19:09:20 +0200

    Time: 18 ms (48.9%), Space: 18.5 MB (48.12%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string longest = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            string odd  = expand(s, i, i);
+            string even = expand(s, i, i+1);
+
+            if(odd.length() > longest.length()) longest = odd;
+            if(even.length() > longest.length()) longest = even;
+        }
+        return longest;
+    }
+
+private:
+    string expand(string& s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return s.substr(left + 1, right - left - 1);
+    }
+};
