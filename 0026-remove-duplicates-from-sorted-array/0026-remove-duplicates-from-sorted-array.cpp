255b2bc846e0561506338f381238f8681330dc4d
jonathan-cpp <130759463+jonathan-cpp@users.noreply.github.com>
2026-05-08 04:09:18 +0200

    Time: 0 ms (100%), Space: 22.6 MB (79.37%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) 
+            return 0;
+
+        int uniqueIdx = 0;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[uniqueIdx] != nums[i]) {
+                nums[++uniqueIdx] = nums[i];
+            }
+        }
+
+        return uniqueIdx + 1;
+    }
+};
