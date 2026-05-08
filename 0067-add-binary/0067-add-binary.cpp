aab2ea00cb55cf782ee6d53d6fd94d3f68b21463
jonathan-cpp <130759463+jonathan-cpp@users.noreply.github.com>
2026-05-08 16:04:09 +0200

    Time: 5 ms (3.89%), Space: 9.1 MB (37.62%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        // Overflow problem 
+        /*
+        return std::bitset<64>(
+            std::bitset<64>(a).to_ulong() +
+            std::bitset<64>(b).to_ulong()
+        ).to_string();
+        */
+        std::string result = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0, sum = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            sum = carry;
+            if (i >= 0) { sum += a[i--] - '0'; }
+            if (j >= 0) { sum += b[j--] - '0'; }
+
+            carry = sum / 2;
+            result += (char)('0' + sum % 2);
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
