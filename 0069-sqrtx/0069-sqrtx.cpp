2914a4e9cecada314b485e17a2dc477870f220c8
jonathan-cpp <130759463+jonathan-cpp@users.noreply.github.com>
2026-05-08 17:11:04 +0200

    Time: 0 ms (100%), Space: 8.8 MB (12.88%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,8 +4,8 @@ public:
         if (x < 2) 
             return x;
 
-        // Newton's method
-        long long guess = x;
+        // Newton's method, with my own estimated guess equation
+        long long guess = 1 << static_cast<int>(std::log2(x) / 2 + 1);
         while (guess * guess > x)
             guess = (guess + x / guess) / 2;
 
