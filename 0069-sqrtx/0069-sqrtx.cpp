a4e42a37623af63badd58a51053ff994378cbb7d
jonathan-cpp <130759463+jonathan-cpp@users.noreply.github.com>
2026-05-08 17:30:12 +0200

    Time: 0 ms (100%), Space: 8.6 MB (49.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,8 +4,24 @@ public:
         if (x < 2) 
             return x;
 
-        // Newton's method, with my own estimated guess equation
+        // Making a good guess in Newton's method can
+        // drastically reduce the number of iterations.
+        /*
+            x=4    (2 bits)   sqrt(x)=2    (1 bit)
+            x=16   (4 bits)   sqrt(x)=4    (2 bits)
+            x=64   (6 bits)   sqrt(x)=8    (3 bits)
+            x=256  (8 bits)   sqrt(x)=16   (4 bits)
+
+            squaring the number, doubles the bits
+            square rooting, halves the bits: sqrt(2^n) = 2^(n/2)
+
+            bits of sqrt(x) ≈ bits of x / 2
+
+            log2(x) = y, meaning x lives around the yth bit
+            +1 saves iterations by overshooting slightly.
+        */
         long long guess = 1 << static_cast<int>(std::log2(x) / 2 + 1);
+        
         while (guess * guess > x)
             guess = (guess + x / guess) / 2;
 
